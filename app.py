e7e5f4872aac28fc3afe46ab8e2d9378ac8ba063
AbinashG <abhi24964@gmail.com>
2025-03-05 00:27:14 +0530

    Local docker testing

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
+    #app.run(host="0.0.0.0",port=5000, debug=True) #for local docker setup
     app.run(debug=True)
